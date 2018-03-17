c2c18f26001a860e9848a879669535b15a08000e
KadenGordon <kaden.m.gordon@gmail.com>
2018-03-17 17:33:27 -0600

    starting to actually drive the intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -17,6 +19,7 @@ public class Intake {
     private Servo rightServo;
     private Servo leftServo;
 
+    private HardwareMap hardwareMap;
     private Telemetry telemetry;
 
     private final double ON_SPEED = 1;
@@ -24,8 +27,12 @@ public class Intake {
 
     private final double OUT_POSITION = 0;
     private final double IN_POSITION = 1;
+    private double servoPosition;
 
-    public Intake(HardwareMap hardwareMap, Telemetry telemetry) {
+    public Intake(OpMode opMode) {
+        this.telemetry = opMode.telemetry;
+        this.hardwareMap = opMode.hardwareMap;
+
         this.rightMotor = hardwareMap.dcMotor.get("m7");
         this.leftMotor = hardwareMap.dcMotor.get("m8");
         this.leftMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -33,8 +40,6 @@ public class Intake {
         this.rightServo = hardwareMap.servo.get("s8");
         this.leftServo = hardwareMap.servo.get("s9");
         this.leftServo.setDirection(Servo.Direction.REVERSE);
-
-        this.telemetry = telemetry;
     }
 
     public void on() {
@@ -62,9 +67,13 @@ public class Intake {
         leftMotor.setPower(speed);
     }
 
-    private void setServoPosition(double position) {
+    public void setServoPosition(double position) { //SET TO PRIVATE ONCE STUFF IS SET
+        this.servoPosition = Range.clip(position, 0, 1);
         rightServo.setPosition(position);
         leftServo.setPosition(position);
     }
 
+    public double getServoPosition() {
+        return servoPosition;
+    }
 }
