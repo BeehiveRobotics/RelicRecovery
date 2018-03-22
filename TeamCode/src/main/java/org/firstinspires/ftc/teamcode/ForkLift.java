aaf27c454ae4a0e34538e3ec1fd9a530667229d6
KadenGordon <kaden.m.gordon@gmail.com>
2018-03-22 17:53:43 -0600

    adjusted limits for forklift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForkLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForkLift.java
@@ -21,7 +21,7 @@ public class ForkLift {
     private DigitalChannel bottomButton;
     private HardwareMap hardwareMap;
     private Telemetry telemetry;
-    private final double CLAW_GRAB_POSITION = 0.6;
+    private final double CLAW_GRAB_POSITION = 0.525;
     private final double CLAW_PUSH_IN_BLOCK_POSITION = 0.85;
     private final double CLAW_OPEN_POSITION = 0.3; //.375 for intake
     private final double CLAW_CLOSE_POSITION = 1;
