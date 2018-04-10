41de013205c240f4fb37d31ff84924a8e747116a
aus <cooler9711@outlook.com>
2018-04-09 21:37:35 -0600

    more useless things removed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRightGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRightGyro.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+@Disabled
 @Autonomous(name = "TestRightGyro", group = "test")
 public class TestRightGyro extends LinearOpMode {
     private Robot robot;
